da1c6deebaecd0e469770c48d8e8affdcc0da1dd
CaoThanhHai <caothanhhai430@gmail.com>
2020-05-14 02:49:01 +0700

    add button

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MyButton text="Red" color="#b81402"/>
+      <MyButton text="Green" color="#014212"/>
+      <MyButton text="Blue" color="#0a6cc2"/>
+      <MyButton text="Undo" color=""/>
+      <MyButton text="Redo" color=""/>
     </View>
   );
 }
 
+const MyButton = (props) => (
+  <TouchableOpacity
+    style={{...styles.button,backgroundColor: props.color}}>
+    <Text style={styles.text}>{props.text}</Text>
+  </TouchableOpacity>
+
+)
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection:'row',
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginStart: 20,
+    marginTop: 40,
   },
+  button: {
+    width: 40,
+    height: 40,
+    alignItems:'center',
+    justifyContent:'center',
+    color:'#ffffff'
+
+  },
+  text:{
+    color: '#ffffff'
+  }
 });
